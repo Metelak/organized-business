525bc420fe491a8b67aa687a464e21502e7a7239
coyote <megan.metelak@gmail.com>
2022-02-20 17:18:09 -0600

    mysql table error

--- db/schema.sql
+++ db/schema.sql
@@ -21,6 +21,6 @@ CREATE TABLE employees (
   last_name VARCHAR(30),
   role_id INTEGER,
   manager_id INTEGER,
-  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-  FOREIGN KEY (manager_id) REFERENCES departments(id) ON DELETE SET NULL
+  CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,20 +7,22 @@ VALUES
 
 INSERT INTO roles (title, salary, dept_id)
 VALUES
-('Occupational Therapist', 83000, 1),
+('Occupational Therapist', 78000, 1),
 ('RN', 60000, 2),
 ('Coding Specialist', 49000, 3),
 ('Staffing Coordinator', 36000, 4),
-('Physical Therapist', 87000, 1),
+('Physical Therapist', 83000, 1),
 ('CNA', 28000, 2),
-('Associate', 48000, 4);
+('Associate', 48000, 4),
+('Rehab Director', 90000, 1),
+('HR Manager', 55000, 4);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-('Peter', 'Linder', 5, 1),
-('Samantha', 'Burton', 7, 4),
-('Charles', 'Smart', 2, 2),
-('Timothy', 'Herd', 4, 4),
-('Brenda', 'Johnston', 3, 3),
-('Karla', 'McLee', 1, 1),
+('Peter', 'Linder', 5, 8),
+('Samantha', 'Burton', 7, 9),
+('Charles', 'Smart', 2, NULL),
+('Timothy', 'Herd', 4, 9),
+('Brenda', 'Johnston', 3, NULL),
+('Karla', 'McLee', 1, 8),
 ('Reed', 'Thorn', 6, 2);
